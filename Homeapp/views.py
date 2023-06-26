e9e4745f36f45259d48dea1f5abb569c38703995
Joshika K <130919538+joshika7@users.noreply.github.com>
2023-06-26 14:49:15 +0530

    Uploaded project files

---
+++ Homeapp/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def sayhello(request):
+    return render(request,'hello.html',{'name':'Shivani','lang':'DJANGO'})
